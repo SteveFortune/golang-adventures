acdd742a33697d6c6d68aa80803dd455611ef7a5
stephen fortune <steve.fortune@icecb.com>
2015-02-04 15:43:12 +0000

    Rather sarcastic hello world

---
+++ HelloWorld/hello.go
@@ -0,0 +1,15 @@
+
+package main
+
+import "fmt"
+
+/**
+* Complicated booting algorithm that initalises our learning curve and listens
+* to sarcasm notifications emitted by the global developer instance.
+*
+* @author Steve Fortune
+* @copyright
+*/
+func main() {
+    fmt.Printf("hello, World\n")
+}
