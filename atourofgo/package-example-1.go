8e8adc2cb6feb450294bfb8afb46ad0e765b1d56
stephen fortune <steve.fortune@icecb.com>
2015-02-05 12:00:59 +0000

    Renamed tour examples dir and added a couple of package examples

---
+++ atourofgo/package-example-1.go
@@ -0,0 +1,14 @@
+
+package atourofgo
+
+/**
+*	Uses grouped import statement...
+*/
+import (
+	"fmt"
+	"math"
+)
+
+func PackageExample1() {
+	fmt.Printf("How you have %g problems", math.Nextafter(1, 10))
+}
---
+++ atourofgo/package-example-2.go
@@ -0,0 +1,12 @@
+
+package atourofgo
+
+/**
+*	Uses individual import statements...
+*/
+import "fmt"
+import "math/rand"
+
+func PackageExample2() {
+	fmt.Printf("How you have %g problems", rand.Intn(10))
+}
